99c6e01d1837ec8c39b6a88c4f9a5eb165952c07
Sam Grogan <sam@fusionmedia.com>
2020-07-05 08:17:19 -0700

    Project setup

---
+++ InfLibrary/InfSerializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF.Net {
+    class InfSerializer {
+
+        Task<InfFile> DeserializeAsync(Stream InputStream) {
+            return null;
+        }
+
+        Task SerializeAsync(InfFile InputFile) {
+            return null;
+        }
+    }
+}
---
+++ InfTest/InfSerializerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using INF.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace INF.Net.Test {
+    [TestClass]
+    public class InfSerializerTests {
+        [TestMethod]
+        public void DeserializeTest() {
+            // Get a list of the INF files in te Windows\INF folder
+            string windowsInfFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), @"\INF");
+            List<string> infFileNames = Directory.EnumerateFiles(windowsInfFolder, "*.inf").ToList<string>();
+
+            // Try to deserialize each file
+            foreach (string infFileName in infFileNames) {
+
+            }
+
+        }
+    }
+}
