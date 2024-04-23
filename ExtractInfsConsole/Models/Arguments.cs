c0acf0a9da98c9934d8a76f568b8bd69f97bcc01
Sam Grogan <samx.grogan@intel.com>
2024-04-23 09:37:25 -0700

    Add placeholder for console app

---
+++ ExtractInfsConsole/Models/Arguments.cs
@@ -0,0 +1,47 @@
+﻿namespace ExtractInfsConsole.Models {
+    internal class Arguments {
+        #region Properties
+
+        public string SourceFolder { get; init; }
+
+        public string OutputFolder { get; init; }
+
+        #endregion Properties
+
+        #region Public Methods
+
+        // Initialze the arguments based on what was passed in from the command line
+        public static Arguments? Create(string[] args) {
+            if (args.Length != 2) {
+                Console.WriteLine($"Usage: ExtractInfsConsole.exe <source folder> <output folder>");
+                return null;
+            }
+
+            string source = args[0];
+            if (!Directory.Exists(source)) {
+                Console.WriteLine($"Source folder '{source}' does not exist.");
+                return null;
+            }
+
+            string output = args[1];
+            if (!Directory.Exists(output)) {
+                Console.WriteLine($"Output folder '{output}' does not exist.");
+                return null;
+            }
+
+            return new Arguments(source, output);
+        }
+
+
+        #endregion Public Methods
+
+        #region Internal Methods
+
+        private Arguments(string source, string output) {
+            SourceFolder = source;
+            OutputFolder = output;
+        }
+
+        #endregion Internal Methods
+    }
+}
---
+++ ExtractInfsConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using ExtractInfsConsole.Models;
+
+namespace ExtractInfsConsole {
+    internal class Program {
+        static void Main(string[] args) {
+            Arguments? arguments = Arguments.Create(args);
+
+            if (arguments != null) {
+                // Get the list of files in the source directory
+                string[] fileNames = Directory.GetFiles(arguments.SourceFolder);
+
+                foreach (string fileName in fileNames) {
+
+
+
+                }
+            }
+        }
+    }
+}
